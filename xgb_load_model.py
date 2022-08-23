31d9575436a37a06232f9d8605c7982174694f48
Belabbaci A.Nazim <30374106+NazimBL@users.noreply.github.com>
2022-08-23 16:58:00 -0400

    Create xgb_load_model.py

---
+++ xgb_load_model.py
@@ -0,0 +1,34 @@
+import pickle
+import xgboost as xgb
+import pandas as pd
+
+#kmers function
+def getKmers(sequence, size=6):
+    return [sequence[x:x+size].lower() for x in range(len(sequence) - size + 1)]
+
+##preprocessing
+gata3_data=pd.read_table("gata3_er_test")
+
+
+##replace seuence column with kmers words
+gata3_data['kmers'] = gata3_data.apply(lambda x: getKmers(x['sequence']), axis=1)
+gata3_data = gata3_data.drop('sequence', axis=1)
+
+kmers_texts = list(gata3_data['kmers'])
+for item in range(len(kmers_texts)):
+    kmers_texts[item] = ' '.join(kmers_texts[item])
+
+loaded_vectorizer = pickle.load(open('vectorizer_gataless.pickle', 'rb'))
+X = loaded_vectorizer.transform(kmers_texts)
+
+print(X.shape)
+model = xgb.XGBClassifier()
+model.load_model("xgb_model_gataless.json")
+
+count=0
+y_pred=model.predict(X)
+print(y_pred)
+for result in y_pred:
+    if(result==1): count = count +1
+    print(str(result))
+print(count)
